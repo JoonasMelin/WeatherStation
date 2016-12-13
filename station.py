15cbbc28542aaadfccf6c109afc71c5c6a2c9e8b
Joonas Melin <joonas.melin@tut.fi>
2016-12-13 00:35:26 +0000

    Error handling for devices

--- station.py
+++ station.py
@@ -103,23 +103,27 @@ def setup():
     GPIO.setup(pull_up_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)
     GPIO.setup(humidity_pin, GPIO.IN)
     GPIO.setup(light_pin, GPIO.IN)
-    time.sleep(0.005)
+    time.sleep(2)
 
 def read_temperature_from(sensor_id):
-    # Open the file that we viewed earlier so that python can see what is in it. Replace the serial number as before. 
-    tfile = open(("/sys/bus/w1/devices/%s/w1_slave"%sensor_id)) 
-    # Read all of the text in the file. 
-    text = tfile.read() 
-    # Close the file now that the text has been read. 
-    tfile.close() 
-    # Split the text with new lines (\n) and select the second line. 
-    secondline = text.split("\n")[1] 
-    # Split the line into words, referring to the spaces, and select the 10th word (counting from 0). 
-    temperaturedata = secondline.split(" ")[9] 
-    # The first two characters are "t=", so get rid of those and convert the temperature from a string to a number. 
-    temperature = float(temperaturedata[2:]) 
-    # Put the decimal point in the right place and display it. 
-    temperature = temperature / 1000
+    # Open the file that we viewed earlier so that python can see what is in it. Replace the serial number as before.
+    temperature = 0
+    try:
+        tfile = open(("/sys/bus/w1/devices/%s/w1_slave"%sensor_id)) 
+        # Read all of the text in the file. 
+        text = tfile.read() 
+        # Close the file now that the text has been read. 
+        tfile.close() 
+        # Split the text with new lines (\n) and select the second line. 
+        secondline = text.split("\n")[1] 
+        # Split the line into words, referring to the spaces, and select the 10th word (counting from 0). 
+        temperaturedata = secondline.split(" ")[9] 
+        # The first two characters are "t=", so get rid of those and convert the temperature from a string to a number. 
+        temperature = float(temperaturedata[2:]) 
+        # Put the decimal point in the right place and display it. 
+        temperature = temperature / 1000
+    except:
+        print("Could not read sensor:", sys.exc_info()[0])
 
     #print("temp from %s is %s"% (sensor_id, temperature))
     return temperature
